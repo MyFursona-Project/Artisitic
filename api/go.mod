b82d1da64b3da7718b83101aa2054695c49a8469
rhydderchc <rhydderchc@gmail.com>
2021-09-16 22:03:13 -0500

    added api spreading paradigm

--- api/go.mod
+++ api/go.mod
@@ -2,4 +2,7 @@ module artistically/api
 
 go 1.13
 
-require github.com/gofiber/fiber/v2 v2.18.0 // indirect
+require (
+	github.com/gofiber/fiber/v2 v2.18.0
+	go.mongodb.org/mongo-driver v1.7.2 // indirect
+)
--- api/main.go
+++ api/main.go
@@ -1,13 +1,17 @@
 package main
 
 import (
-	"github.com/gofiber/fiber/v2"
+	"artistically/api/routes"
 	"log"
+
+	"github.com/gofiber/fiber/v2"
 )
 
 func main() {
 
 	app := fiber.New()
+
+	routes.SpreadRoutes(app)
 
 	log.Fatal(app.Listen(":3000"))
 
